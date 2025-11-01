c38afd1b4c8c214a7596b19aacb789c21891788e
Alexis Lewis <142439982+alexisnrlewis@users.noreply.github.com>
2025-11-01 15:08:07 -0400

    Create Coin.gd

---
+++ Scripts/Coin.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+func _ready() -> void:
+	connect("body_entered", Callable(self, "_on_body_entered"))
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("player"):
+		body.collect_coin(10)
+		queue_free()  # remove coin
